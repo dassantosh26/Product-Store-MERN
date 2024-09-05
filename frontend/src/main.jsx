66604e5a7bf9211e368155b91a6215bd1a367155
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-09-05 20:56:32 +0530

    Setup frontend with vite also installed dependency like Chakra-Ui,Zustand,react-icon

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+import { StrictMode } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);

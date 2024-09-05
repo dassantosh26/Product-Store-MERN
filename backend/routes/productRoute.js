f49b6a785ac731432c19c0dc4d56597abe90bdd6
Santosh Kumar Das <santoshdas.mits@gmail.com>
2024-09-05 20:35:48 +0530

    Organised code Putting all the end point in Controllers folder

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,76 +1,21 @@
 /** @format */
 
 import express from "express";
-import mongoose from "mongoose";
-import Product from "../models/productModel.js";
+import {
+  createProduct,
+  deleteProduct,
+  getProducts,
+  updateProduct,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-//GET
+router.get("/", getProducts);
 
-router.get("/", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ success: true, data: products });
-  } catch (error) {
-    console.error(`Error in creating product: ${error.message}`);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
+router.post("/", createProduct);
 
-//POST
-router.post("/", async (req, res) => {
-  const product = req.body; // user will send this data
+router.delete("/:id", deleteProduct);
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.error(`Error in creating product:${error.message} `);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-//DELETE
-
-router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  // console.log(id)
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product Deleted" });
-  } catch (error) {
-    console.error(`Error in deleting product: ${error.message}`);
-    res.status(404).json({ success: false, message: "Product not found" });
-  }
-});
-
-//PUT
-router.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  // console.log(id)
-  const product = req.body;
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res
-      .status(404)
-      .json({ success: false, message: "Invalid Product Id" });
-  }
-
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-      new: true,
-    });
-    res.status(201).json({ success: true, data: updatedProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
+router.put("/:id", updateProduct);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 /** @format */
 
 import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import productRoute from "./routes/productRoute.js"
+import productRoute from "./routes/productRoute.js";
 
 const app = express();
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json()); //middleware allow us to accept json data in the req.body
 
-app.use("/api/products",productRoute)
+app.use("/api/products", productRoute);
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log(`Server started at http://localhost:5000`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
